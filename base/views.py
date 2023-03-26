3c885398cf7a244046b3b39169e308a370b038e5
Ojas Mutreja <helloojasmutreja@gmail.com>
2023-03-27 01:35:05 +0530

    Room Model Setup

--- base/views.py
+++ base/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn python!'},
-    {'id': 2, 'name': 'Lets learn SQLite!'},
-    {'id': 3, 'name': 'Lets try Ruby on Rails!'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python!'},
+#     {'id': 2, 'name': 'Lets learn SQLite!'},
+#     {'id': 3, 'name': 'Lets try Ruby on Rails!'},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', {'rooms': rooms})
 
 # end def
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
